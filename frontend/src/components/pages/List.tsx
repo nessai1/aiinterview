e7e41fbd9c534fee3bc8477d66e33b79c921dd3c
Daniel Verevkin <werowkin@gmail.com>
2025-01-23 13:55:37 +0200

    frontend: List component start

--- frontend/src/components/pages/List.tsx
+++ frontend/src/components/pages/List.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import {Separator} from "@/components/ui/separator.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 
 const List: React.FC = () => {
     return (
-        <>
-            list
-        </>
+        <div className={"flex flex-col w-full"}>
+            <div className={"w-full flex p-5"}>
+                <Button>Новое интервью</Button>
+            </div>
+            <Separator/>
+            <div>
+                List...
+            </div>
+        </div>
     );
 }
 
