3e6eb9f16a1d081580b7253732a7a65c722359e6
nessai1 <werowkin@gmail.com>
2025-01-27 13:12:47 +0200

    backend: доменная область

---
+++ backend/internal/domain/interview.go
@@ -0,0 +1,23 @@
+package domain
+
+import "time"
+
+type Interview struct {
+	Title     string
+	Timing    time.Duration
+	StartTime time.Time
+	Topics    []Topic
+}
+
+type TopicGrade string
+
+const (
+	GradeJunior TopicGrade = "junior"
+	GradeMiddle TopicGrade = "middle"
+	GradeSenior TopicGrade = "senior"
+)
+
+type Topic struct {
+	Name  string
+	Grade TopicGrade
+}
