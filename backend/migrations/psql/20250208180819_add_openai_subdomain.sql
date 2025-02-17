19236fab92898fa01f327fdd76940fc7a0615f21
Daniil Verevkin <d.verevkin@bitrix.ru>
2025-02-17 15:00:18 +0200

    backend: Фикс миграции ассистентов

--- backend/migrations/psql/20250208180819_add_openai_subdomain.sql
+++ backend/migrations/psql/20250208180819_add_openai_subdomain.sql
@@ -3,15 +3,15 @@
 CREATE TABLE IF NOT EXISTS assistants (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     external_id VARCHAR(255) NOT NULL,
-    model VARCHAR(80) NOT NULL,
+    model VARCHAR(80) NOT NULL
 );
 
-UPDATE TABLE interview ADD COLUMN thread_id VARCHAR(255);
+ALTER TABLE interview ADD COLUMN thread_id VARCHAR(255);
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS assistants;
-ALTER TABLE IF EXISTS interview DROP COLUMN IF EXISTS thread_id;
+ALTER TABLE interview DROP COLUMN IF EXISTS thread_id;
 -- +goose StatementEnd
